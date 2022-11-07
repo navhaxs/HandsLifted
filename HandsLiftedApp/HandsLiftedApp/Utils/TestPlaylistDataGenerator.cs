575baa8891fa8c5661e646f5db81fd52308ebd79
Jeremy Wong <navhaxs@hotmail.com>
2022-11-08 08:42:04 +1100

    fresh build on t450s

--- HandsLiftedApp/HandsLiftedApp/Utils/TestPlaylistDataGenerator.cs
+++ HandsLiftedApp/HandsLiftedApp/Utils/TestPlaylistDataGenerator.cs
@@ -19,39 +19,43 @@ namespace HandsLiftedApp.Utils
             Playlist<PlaylistStateImpl, ItemStateImpl> playlist = new Playlist<PlaylistStateImpl, ItemStateImpl>();
             //return playlist;
             var rnd = new Random();
-            var songs = Directory.GetFiles(@"C:\VisionScreens\TestSongs", "*.*", SearchOption.AllDirectories)
+
+            if (Directory.Exists(@"C:\VisionScreens\TestSongs"))
+            {
+                var songs = Directory.GetFiles(@"C:\VisionScreens\TestSongs", "*.*", SearchOption.AllDirectories)
                 .Where(s => s.ToLower().EndsWith(".txt"))
                 .OrderBy(x => rnd.Next()).Take(4)
                 ;
-            foreach (var f in songs)
-            {
-                playlist.Items.Add(SongImporter.ImportSongFromTxt(f));
-            }
-
-            SlidesGroupItem<ItemStateImpl, ItemAutoAdvanceTimerStateImpl> slidesGroup = new SlidesGroupItem<ItemStateImpl, ItemAutoAdvanceTimerStateImpl>();
-            try
-            {
-                var images = Directory.GetFiles(@"C:\VisionScreens\TestImages", "*.*", SearchOption.AllDirectories)
-                                .Where(s => s.ToLower().EndsWith(".png") || s.ToLower().EndsWith(".jpg") || s.ToLower().EndsWith(".jpeg") || s.ToLower().EndsWith(".mp4"))
-                                .OrderBy(x => x, new NaturalSortStringComparer(StringComparison.Ordinal));
-                int i = 0;
-                foreach (var f in images)
+                foreach (var f in songs)
                 {
-                    if (f.EndsWith(".mp4"))
-                    {
-                        slidesGroup._Slides.Add(new VideoSlide<VideoSlideStateImpl>(f) { Index = i });
-                    }
-                    else
-                    {
-                        slidesGroup._Slides.Add(new ImageSlide<ImageSlideStateImpl>(f) { Index = i });
-                    }
-                    i++;
+                    playlist.Items.Add(SongImporter.ImportSongFromTxt(f));
                 }
-            }
-            catch { }
-            playlist.Items.Add(slidesGroup);
 
-            slidesGroup.Title = "Announcements";
+                SlidesGroupItem<ItemStateImpl, ItemAutoAdvanceTimerStateImpl> slidesGroup = new SlidesGroupItem<ItemStateImpl, ItemAutoAdvanceTimerStateImpl>();
+                try
+                {
+                    var images = Directory.GetFiles(@"C:\VisionScreens\TestImages", "*.*", SearchOption.AllDirectories)
+                                    .Where(s => s.ToLower().EndsWith(".png") || s.ToLower().EndsWith(".jpg") || s.ToLower().EndsWith(".jpeg") || s.ToLower().EndsWith(".mp4"))
+                                    .OrderBy(x => x, new NaturalSortStringComparer(StringComparison.Ordinal));
+                    int i = 0;
+                    foreach (var f in images)
+                    {
+                        if (f.EndsWith(".mp4"))
+                        {
+                            slidesGroup._Slides.Add(new VideoSlide<VideoSlideStateImpl>(f) { Index = i });
+                        }
+                        else
+                        {
+                            slidesGroup._Slides.Add(new ImageSlide<ImageSlideStateImpl>(f) { Index = i });
+                        }
+                        i++;
+                    }
+                }
+                catch { }
+                playlist.Items.Add(slidesGroup);
+
+                slidesGroup.Title = "Announcements";
+            }
 
             return playlist;
         }
