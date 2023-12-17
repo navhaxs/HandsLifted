b800944d0213d3c7853743a8c151b5b0331ccc00
Jeremy Wong <navhaxs@hotmail.com>
2023-12-17 21:24:39 +1100

    test custom axaml slide

--- HandsLiftedApp/Data/Models/Slides/CustomSlide.cs
+++ HandsLiftedApp/Data/Models/Slides/CustomSlide.cs
@@ -1,7 +1,11 @@
 ﻿using HandsLiftedApp.Data.Slides;
+using System.Xml.Serialization;
+using System;
 
 namespace HandsLiftedApp.Data.Models.Slides
 {
+    [XmlRoot(Namespace = Constants.Namespace)]
+    [Serializable]
     public class CustomSlide : Slide
     {
         public override string? SlideLabel => "abc";
