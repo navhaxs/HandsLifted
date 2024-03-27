a3c511950fd5430cd46cd5498b19205fafffb0c7
Jeremy Wong <navhaxs@hotmail.com>
2024-03-27 21:44:14 +1100

    :sparkles: add both syncfusion + netoffice powerpoint importer - further files

---
+++ HandsLiftedApp.Importer.PowerPoint.Bridge/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HandsLiftedApp.Importer.PowerPoint.Bridge;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HandsLiftedApp.Importer.PowerPointInteropData/ImportTask.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+
+namespace HandsLiftedApp.Importer.PowerPointInteropData;
+
+[ProtoContract]
+public class ImportTask
+{
+    [ProtoMember(1)]
+    public string PPTXFilePath { get; set; }
+
+    public string OutputDirectory
+    {
+        get
+        {
+            return Path.GetDirectoryName(PPTXFilePath);
+        }
+    }
+}
