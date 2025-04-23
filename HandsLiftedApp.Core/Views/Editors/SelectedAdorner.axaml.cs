0d559a8bec8a47d40ae6a64296799862afb9889e
Jeremy Wong <navhaxs@hotmail.com>
2025-04-23 20:55:52 +1000

    implement snap to centre, snap to horizontal centre, snap to vertical centre

--- HandsLiftedApp.Core/Views/Editors/SelectedAdorner.axaml.cs
+++ HandsLiftedApp.Core/Views/Editors/SelectedAdorner.axaml.cs
@@ -4,7 +4,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.PanAndZoom;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using Point = Avalonia.Point;
 
 namespace PerspectiveDemo
 {
@@ -26,7 +30,7 @@ namespace PerspectiveDemo
         private Thumb? _topRight;
         private Thumb? _bottomLeft;
         private Thumb? _bottomRight;
-        private Thumb? _centre;
+        // private Thumb? _centre;
         private double _leftOffset;
         private double _topOffset;
 
@@ -40,6 +44,57 @@ namespace PerspectiveDemo
         {
             get => GetValue(ZoomBorderProperty);
             set => SetValue(ZoomBorderProperty, value);
+        }
+
+        private Line? verticalLine = null;
+        private Line? horizontalLine = null;
+        private void UpdateSnapLines(bool showX, bool showY)
+        {
+            // <Line StartPoint="960,0" EndPoint="960,1080" Stroke="Red" StrokeThickness="2"/>
+            // <Line StartPoint="0,540" EndPoint="1920,540" Stroke="Red" StrokeThickness="2"/>
+            var rootCanvas = ZoomBorder.GetControl<Canvas>("Root");
+            if (showX)
+            {
+                if (horizontalLine == null)
+                {
+                    horizontalLine = new Line()
+                    {
+                        StartPoint = new Point(0, 540), EndPoint = new Point(1920, 540), Stroke = Brushes.Red,
+                        StrokeThickness = 2, Name = "HorizontalLine"
+                    };
+                    rootCanvas.Children.Add(horizontalLine);
+                }
+            }
+            else
+            {
+                if (horizontalLine != null)
+                {
+                    rootCanvas.Children.Remove(horizontalLine);
+                    horizontalLine = null;
+                }
+            }
+            if (showY)
+            {
+                if (verticalLine == null)
+                {
+                    verticalLine = new Line() { StartPoint = new Point(960, 0), EndPoint = new Point(960, 1080), Stroke = Brushes.Red, StrokeThickness = 2, Name = "VerticalLine"};
+                    rootCanvas.Children.Add(verticalLine);
+                }
+            }
+            else
+            {
+                if (verticalLine != null)
+                {
+                    rootCanvas.Children.Remove(verticalLine);
+                    verticalLine = null;
+                }
+            }
+        }
+
+        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            UpdateSnapLines(false, false);
+            base.OnDetachedFromLogicalTree(e);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
@@ -62,7 +117,7 @@ namespace PerspectiveDemo
             _topRight = e.NameScope.Find<Thumb>("PART_TopRight");
             _bottomLeft = e.NameScope.Find<Thumb>("PART_BottomLeft");
             _bottomRight = e.NameScope.Find<Thumb>("PART_BottomRight");
-            _centre = e.NameScope.Find<Thumb>("PART_Centre");
+            // _centre = e.NameScope.Find<Thumb>("PART_Centre");
 
             // pass-through mouse wheel events
             this.PointerWheelChanged += OnPointerWheelChanged;
@@ -144,6 +199,12 @@ namespace PerspectiveDemo
 
         private Point? lastPosition;
 
+        private void ApplyDeltaToThumb(Thumb targetThumb, double deltaX, double deltaY)
+        {
+            Canvas.SetLeft(targetThumb, Canvas.GetLeft(targetThumb) + deltaX);
+            Canvas.SetTop(targetThumb, Canvas.GetTop(targetThumb) + deltaY);
+        }
+        
         private void OnPointerMovedDrag(object? sender, PointerEventArgs e)
         {
             var visual = GetValue(AdornerLayer.AdornedElementProperty);
@@ -155,39 +216,93 @@ namespace PerspectiveDemo
                 var deltaX = newPosition.X - lastPosition.Value.X;
                 var deltaY = newPosition.Y - lastPosition.Value.Y;
 
-                Canvas.SetLeft(_top, Canvas.GetLeft(_top) + deltaX);
-                Canvas.SetLeft(_bottom, Canvas.GetLeft(_bottom) + deltaX);
+                var previousRect = GetRect();
+                var draggedRect = new Rect(previousRect.X + deltaX, previousRect.Y + deltaY, previousRect.Width, previousRect.Height);
 
-                Canvas.SetLeft(_left, Canvas.GetLeft(_left) + deltaX);
-                Canvas.SetLeft(_right, Canvas.GetLeft(_right) + deltaX);
+                // Calculate the center of the canvas
+                var canvasCenterX = ZoomBorder.GetControl<Canvas>("Root").Bounds.Width / 2;
+                var canvasCenterY = ZoomBorder.GetControl<Canvas>("Root").Bounds.Height / 2;
 
-                Canvas.SetLeft(_topLeft, Canvas.GetLeft(_topLeft) + deltaX);
-                Canvas.SetLeft(_bottomLeft, Canvas.GetLeft(_bottomLeft) + deltaX);
+                // Calculate the current position of the dragged element
+                var elementCenterX = draggedRect.X + (draggedRect.Width / 2);
+                var elementCenterY = draggedRect.Y + (draggedRect.Height / 2);
+                
+                // Define a snapping threshold
+                const double snappingThreshold = 10.0;
 
-                Canvas.SetLeft(_topRight, Canvas.GetLeft(_topRight) + deltaX);
-                Canvas.SetLeft(_bottomRight, Canvas.GetLeft(_bottomRight) + deltaX);
-
-                Canvas.SetTop(_top, Canvas.GetTop(_top) + deltaY);
-                Canvas.SetTop(_bottom, Canvas.GetTop(_bottom) + deltaY);
-
-                Canvas.SetTop(_left, Canvas.GetTop(_left) + deltaY);
-                Canvas.SetTop(_right, Canvas.GetTop(_right) + deltaY);
-
-                Canvas.SetTop(_topLeft, Canvas.GetTop(_topLeft) + deltaY);
-                Canvas.SetTop(_topRight, Canvas.GetTop(_topRight) + deltaY);
-
-                Canvas.SetTop(_bottomLeft, Canvas.GetTop(_bottomLeft) + deltaY);
-                Canvas.SetTop(_bottomRight, Canvas.GetTop(_bottomRight) + deltaY);
+                // Check if the element is near the center
+                if (Math.Abs(elementCenterX - canvasCenterX) <= snappingThreshold &&
+                    Math.Abs(elementCenterY - canvasCenterY) <= snappingThreshold)
+                {
+                    // Snap to the center
+                    draggedRect = new Rect(canvasCenterX - (draggedRect.Width / 2),  canvasCenterY - (draggedRect.Height / 2), previousRect.Width, previousRect.Height);
+                    UpdateSnapLines(true, true);
+                } 
+                else if (Math.Abs(elementCenterX - canvasCenterX) <= snappingThreshold)
+                {
+                    // Snap to the vertical axis
+                    draggedRect = new Rect(canvasCenterX - (draggedRect.Width / 2), draggedRect.Y, previousRect.Width, previousRect.Height);
+                    UpdateSnapLines(false, true);
+                }
+                else if (Math.Abs(elementCenterY - canvasCenterY) <= snappingThreshold)
+                {
+                    // Snap to the horizontal axis
+                    draggedRect = new Rect(draggedRect.X, canvasCenterY - (draggedRect.Height / 2), previousRect.Width, previousRect.Height);
+                    UpdateSnapLines(true, false);
+                }
+                else
+                {
+                    UpdateSnapLines(false, false);
+                }
+                // new[] { _top, _bottom, _left, _right, _topLeft, _bottomLeft, _topRight, _bottomRight }
+                //     .ToList()
+                //     .ForEach(thumb => ApplyDeltaToThumb(thumb, deltaX, deltaY));
+                
+                /*
+                 * ------> X
+                 * |
+                 * |
+                 * V
+                 * Y
+                 */
+                
+                /*
+                 * x-----x-----x
+                 * |           |
+                 * x           x
+                 * |           |
+                 * x-----x-----x
+                 */
+                
+                Canvas.SetLeft(_topLeft, draggedRect.X);
+                Canvas.SetTop(_topLeft, draggedRect.Y);
+                Canvas.SetLeft(_bottomRight, draggedRect.X + draggedRect.Width);
+                Canvas.SetTop(_bottomRight, draggedRect.Y + draggedRect.Height);
+                Canvas.SetLeft(_top, draggedRect.X + draggedRect.Width / 2);
+                Canvas.SetTop(_top, draggedRect.Y);
+                Canvas.SetLeft(_topRight, draggedRect.X + draggedRect.Width);
+                Canvas.SetTop(_topRight, draggedRect.Y);
+                Canvas.SetLeft(_left, draggedRect.X);
+                Canvas.SetTop(_left, draggedRect.Y + draggedRect.Height / 2);
+                Canvas.SetLeft(_bottomLeft, draggedRect.X);
+                Canvas.SetTop(_bottomLeft, draggedRect.Y + draggedRect.Height);
+                Canvas.SetLeft(_bottom, draggedRect.X + draggedRect.Width / 2);
+                Canvas.SetTop(_bottom, draggedRect.Y + draggedRect.Height);
+                Canvas.SetLeft(_right, draggedRect.X + draggedRect.Width);
+                Canvas.SetTop(_right, draggedRect.Y + draggedRect.Height / 2);
 
                 var rect = GetRect();
-
                 UpdateDrag(rect);
-
+             
                 if (Control is { })
                 {
                     UpdateControl(Control,
                         rect.Inflate(new Thickness(-_leftOffset, -_topOffset, _leftOffset, _topOffset)));
                 }
+            }
+            else
+            {
+                UpdateSnapLines(false, false);
             }
 
             lastPosition = newPosition;
@@ -255,10 +370,6 @@ namespace PerspectiveDemo
                 _drag.Height = rect.Height;
             }
         }
-
-        private double GetVectorX(VectorEventArgs e) => e.Vector.X; // ZoomBorder.ZoomX;
-
-        private double GetVectorY(VectorEventArgs e) => e.Vector.Y; // ZoomBorder.ZoomY;
 
         private void OnPointerMovedTop(object? sender, PointerEventArgs e)
         {
