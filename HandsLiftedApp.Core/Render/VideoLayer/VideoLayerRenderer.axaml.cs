4dbcda466324b656cbc7a8547dd6965c944274b9
Jeremy Wong <navhaxs@hotmail.com>
2025-05-10 11:10:20 +1000

    Move MPV Video playback surface into a separate layer below slides. Seems to improve reliability of un/re-mounting issue.

---
+++ HandsLiftedApp.Core/Render/VideoLayer/VideoLayerRenderer.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HandsLiftedApp.Core.Render.VideoLayer
+{
+    public partial class VideoLayerRenderer : UserControl
+    {
+        public VideoLayerRenderer()
+        {
+            InitializeComponent();
+        }
+    }
+}
