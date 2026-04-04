f88c60ae3a39d44749d40df789fe1e2406eddecb
Jeremy Wong <navhaxs@hotmail.com>
2026-04-04 13:57:46 +1100

    :art: welcome window ui: make recent playlists section grouping more obvious

--- HandsLiftedApp.Core/ViewModels/WelcomeWindowViewModel.cs
+++ HandsLiftedApp.Core/ViewModels/WelcomeWindowViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Reactive;
+using Avalonia.Controls;
 using ReactiveUI;
 
 namespace HandsLiftedApp.Core.ViewModels
@@ -9,12 +9,23 @@ namespace HandsLiftedApp.Core.ViewModels
     {
         public MainViewModel _parent;
 
-        public WelcomeWindowViewModel(MainViewModel parent)
+        public WelcomeWindowViewModel()
+        {
+            if (Design.IsDesignMode)
+            {
+                RecentPlaylists.Add(new RecentPlaylistEntry() { FileName = "Sunday Service.hlsx", FilePath = @"C:\Playlists\Sunday Service.hlsx" });
+                RecentPlaylists.Add(new RecentPlaylistEntry() { FileName = "Youth Night.hlsx", FilePath = @"C:\Playlists\Youth Night.hlsx" });
+                RecentPlaylists.Add(new RecentPlaylistEntry() { FileName = "Wedding.hlsx", FilePath = @"C:\Playlists\Wedding.hlsx" });
+            }
+        }
+
+        public WelcomeWindowViewModel(MainViewModel parent) : this()
         {
             _parent = parent;
 
-            if (parent.settings.RecentPlaylistFullPathsList is not null)
+            if (parent?.settings?.RecentPlaylistFullPathsList is not null)
             {
+                RecentPlaylists.Clear();
                 foreach (var se in parent.settings.RecentPlaylistFullPathsList)
                 {
                     RecentPlaylists.Add(new RecentPlaylistEntry() { FilePath = se, FileName = Path.GetFileName(se) });
