92c7bfd9f693761c8596aefc8c88fb0e1b5d33fb
Jeremy Wong <navhaxs@hotmail.com>
2026-05-26 22:08:53 +1000

    fix: dispose SongSlideView subscription on detach, document theme change propagation

--- HandsLiftedApp.Core/Views/Designer/SongSlideView.axaml.cs
+++ HandsLiftedApp.Core/Views/Designer/SongSlideView.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reactive.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using HandsLiftedApp.Core.Render.Skia;
 using HandsLiftedApp.Core.Render.Skia.Builders;
@@ -25,11 +26,19 @@ public partial class SongSlideView : UserControl
 
         if (DataContext is not SongSlideInstance slide) return;
 
+        // Theme inner-property changes trigger this subscription indirectly:
+        // SongSlideInstance reassigns Theme whenever DefaultTheme properties change.
         _subscription = slide
             .WhenAnyValue(s => s.Text, s => s.Theme)
             .Subscribe(_ => RebuildSpec(slide));
 
         RebuildSpec(slide);
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        _subscription?.Dispose();
+        base.OnDetachedFromVisualTree(e);
     }
 
     private void RebuildSpec(SongSlideInstance slide)
