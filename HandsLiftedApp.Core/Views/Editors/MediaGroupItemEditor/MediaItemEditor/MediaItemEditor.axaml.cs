3ec2b2acacc3f98c5ddf0e32a69c185fdbac0366
Jeremy Wong <navhaxs@hotmail.com>
2026-06-10 23:36:08 +1000

    MediaItemEditor: implement file picker button

--- HandsLiftedApp.Core/Views/Editors/MediaGroupItemEditor/MediaItemEditor/MediaItemEditor.axaml.cs
+++ HandsLiftedApp.Core/Views/Editors/MediaGroupItemEditor/MediaItemEditor/MediaItemEditor.axaml.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
-using Avalonia.Threading;
-using HandsLiftedApp.Data.Data.Models.Slides;
 using HandsLiftedApp.Data.Models.Items;
-using HandsLiftedApp.Data.Models.SlideElement;
-using Serilog;
 
 namespace HandsLiftedApp.Core.Views.Editors.FreeText
 {
@@ -18,13 +12,37 @@ namespace HandsLiftedApp.Core.Views.Editors.FreeText
         public MediaItemEditor()
         {
             InitializeComponent();
-            
+
             if (Design.IsDesignMode)
             {
                 this.DataContext = new MediaGroupItem.MediaItem() { SourceMediaFilePath = "avares://HandsLiftedApp.Core/Assets/DefaultTheme/VisionScreens_1440_placeholder.png" };
             }
-            
         }
 
+        private async void BrowseButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            var topLevel = TopLevel.GetTopLevel(this);
+            if (topLevel == null) return;
+
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Select Media File",
+                AllowMultiple = false,
+                FileTypeFilter = new[]
+                {
+                    new FilePickerFileType("Image files") { Patterns = new[] { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp", "*.webp" } },
+                    new FilePickerFileType("Video files") { Patterns = new[] { "*.mp4", "*.mov", "*.avi", "*.mkv", "*.webm" } },
+                    new FilePickerFileType("All files") { Patterns = new[] { "*.*" } },
+                }
+            });
+
+            var filePath = files.Count > 0 ? files[0].TryGetLocalPath() : null;
+            if (filePath == null) return;
+
+            if (DataContext is MediaGroupItem.MediaItem mediaItem)
+            {
+                mediaItem.SourceMediaFilePath = filePath;
+            }
+        }
     }
 }
