24975742ba5a93e268e5d9a6b9ade3af3cd68c5a
Jeremy Wong <navhaxs@hotmail.com>
2026-06-16 21:23:42 +1000

    add deadzone to library list view item drag event

--- HandsLiftedApp.Core/Views/LibraryView/LibraryPaneView.axaml.cs
+++ HandsLiftedApp.Core/Views/LibraryView/LibraryPaneView.axaml.cs
@@ -1,66 +1,78 @@
 using System;
 using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
-using Castle.Core.Logging;
 using HandsLiftedApp.Core.Models.Library;
 
 namespace HandsLiftedApp.Views.Library
 {
     public partial class LibraryPaneView : UserControl
     {
+        private const double DragThreshold = 4.0;
+        private Point? _dragStart;
+        private LibraryItem? _pendingDragItem;
+        private Control? _pendingDragControl;
+
         public LibraryPaneView()
         {
             InitializeComponent();
-
-            //this.DataContext = new LogoSlide();
         }
 
-        private const string CustomFormat = "application/xxx-avalonia-controlcatalog-custom";
-
-        private async void DockPanel_PointerPressed(object? sender, PointerPressedEventArgs e)
+        private void DockPanel_PointerPressed(object? sender, PointerPressedEventArgs e)
         {
-            if (sender is Control control)
+            if (sender is Control control && control.DataContext is LibraryItem libraryItem)
             {
-                if (control.DataContext is LibraryItem libraryItem)
-                {
-                    var dragData = new DataObject();
-                    var topLevel = TopLevel.GetTopLevel(this);
-                    IStorageFile originalCoverImage = await topLevel.StorageProvider.TryGetFileFromPathAsync(new Uri(libraryItem.FullFilePath));
-                    
-                    dragData.Set(DataFormats.Files, new[] { originalCoverImage });
-
-                    var result = await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy);
-                    Debug.Print("a");
-                    // switch (result)
-                    // {
-                    //     case DragDropEffects.Move:
-                    //         dragState.Text = "Data was moved";
-                    //         break;
-                    //     case DragDropEffects.Copy:
-                    //         dragState.Text = "Data was copied";
-                    //         break;
-                    //     case DragDropEffects.Link:
-                    //         dragState.Text = "Data was linked";
-                    //         break;
-                    //     case DragDropEffects.None:
-                    //         dragState.Text = "The drag operation was canceled";
-                    //         break;
-                    //     default:
-                    //         dragState.Text = "Unknown result";
-                    //         break;
-                    // }
-                }
+                _dragStart = e.GetPosition(null);
+                _pendingDragItem = libraryItem;
+                _pendingDragControl = control;
+                control.PointerMoved += DockPanel_PointerMoved;
+                control.PointerReleased += DockPanel_PointerReleased;
+                e.Pointer.Capture(control);
             }
+        }
+
+        private void DockPanel_PointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            CancelPendingDrag();
+        }
+
+        private void CancelPendingDrag()
+        {
+            if (_pendingDragControl != null)
+            {
+                _pendingDragControl.PointerMoved -= DockPanel_PointerMoved;
+                _pendingDragControl.PointerReleased -= DockPanel_PointerReleased;
+            }
+            _dragStart = null;
+            _pendingDragItem = null;
+            _pendingDragControl = null;
+        }
+
+        private async void DockPanel_PointerMoved(object? sender, PointerEventArgs e)
+        {
+            if (_dragStart == null || _pendingDragItem == null) return;
+
+            var delta = e.GetPosition(null) - _dragStart.Value;
+            if (Math.Abs(delta.X) < DragThreshold && Math.Abs(delta.Y) < DragThreshold) return;
+
+            var item = _pendingDragItem;
+            CancelPendingDrag();
+
+            var dragData = new DataObject();
+            var topLevel = TopLevel.GetTopLevel(this);
+            IStorageFile file = await topLevel.StorageProvider.TryGetFileFromPathAsync(new Uri(item.FullFilePath));
+            dragData.Set(DataFormats.Files, new[] { file });
+
+            await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy);
         }
 
         private void MenuItem_OnClick(object? sender, RoutedEventArgs e)
         {
             if (sender is Control { DataContext: Core.Models.Library.Library library })
                 Process.Start(new ProcessStartInfo(library.Config.Directory) { UseShellExecute = true });
-
         }
     }
 }
--- HandsLiftedApp.Core/Views/LibraryView/LibraryQueryView.axaml.cs
+++ HandsLiftedApp.Core/Views/LibraryView/LibraryQueryView.axaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -13,47 +12,64 @@ namespace HandsLiftedApp.Core.Views.LibraryView
 {
     public partial class LibraryQueryView : UserControl
     {
+        private const double DragThreshold = 4.0;
+        private Point? _dragStart;
+        private LibraryItem? _pendingDragItem;
+        private Control? _pendingDragControl;
+
         public LibraryQueryView()
         {
             AsyncImageLoader.ImageLoader.AsyncImageLoader = new WindowsThumbnailImageLoader();
-            
+
             InitializeComponent();
         }
-        
-        private async void DockPanel_PointerPressed(object? sender, PointerPressedEventArgs e)
-        {
-            if (sender is Control control)
-            {
-                if (control.DataContext is LibraryItem libraryItem)
-                {
-                    var dragData = new DataObject();
-                    var topLevel = TopLevel.GetTopLevel(this);
-                    IStorageFile originalCoverImage = await topLevel.StorageProvider.TryGetFileFromPathAsync((Uri)new Uri(libraryItem.FullFilePath));
-                    
-                    dragData.Set(DataFormats.Files, new[] { originalCoverImage });
 
-                    var result = await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy);
-                    Debug.Print("a");
-                    // switch (result)
-                    // {
-                    //     case DragDropEffects.Move:
-                    //         dragState.Text = "Data was moved";
-                    //         break;
-                    //     case DragDropEffects.Copy:
-                    //         dragState.Text = "Data was copied";
-                    //         break;
-                    //     case DragDropEffects.Link:
-                    //         dragState.Text = "Data was linked";
-                    //         break;
-                    //     case DragDropEffects.None:
-                    //         dragState.Text = "The drag operation was canceled";
-                    //         break;
-                    //     default:
-                    //         dragState.Text = "Unknown result";
-                    //         break;
-                    // }
-                }
+        private void DockPanel_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (sender is Control control && control.DataContext is LibraryItem libraryItem)
+            {
+                _dragStart = e.GetPosition(null);
+                _pendingDragItem = libraryItem;
+                _pendingDragControl = control;
+                control.PointerMoved += DockPanel_PointerMoved;
+                control.PointerReleased += DockPanel_PointerReleased;
+                e.Pointer.Capture(control);
             }
+        }
+
+        private void DockPanel_PointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            CancelPendingDrag();
+        }
+
+        private void CancelPendingDrag()
+        {
+            if (_pendingDragControl != null)
+            {
+                _pendingDragControl.PointerMoved -= DockPanel_PointerMoved;
+                _pendingDragControl.PointerReleased -= DockPanel_PointerReleased;
+            }
+            _dragStart = null;
+            _pendingDragItem = null;
+            _pendingDragControl = null;
+        }
+
+        private async void DockPanel_PointerMoved(object? sender, PointerEventArgs e)
+        {
+            if (_dragStart == null || _pendingDragItem == null) return;
+
+            var delta = e.GetPosition(null) - _dragStart.Value;
+            if (Math.Abs(delta.X) < DragThreshold && Math.Abs(delta.Y) < DragThreshold) return;
+
+            var item = _pendingDragItem;
+            CancelPendingDrag();
+
+            var dragData = new DataObject();
+            var topLevel = TopLevel.GetTopLevel(this);
+            IStorageFile file = await topLevel.StorageProvider.TryGetFileFromPathAsync(new Uri(item.FullFilePath));
+            dragData.Set(DataFormats.Files, new[] { file });
+
+            await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Copy);
         }
 
         private void InputElement_OnKeyDown(object? sender, KeyEventArgs e)
